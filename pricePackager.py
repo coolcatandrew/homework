3ba6827da67df93f2f08fb673adaf1ada3ffd421
coolcatandrew <liandrew@live.com>
2017-11-02 16:57:52 -0400

    outline

---
+++ pricePackager.py
@@ -0,0 +1,15 @@
+#homework from Kira Talent
+#Andrew Li
+#create a library to help take existing products, repackaging them for sale at electronics stores
+
+
+
+
+
+#function to take in the base item price, the number of people who need to work on the item, and the item type and return a float as the final price after the markup
+def calculateMarkup(basePrice, numberOfPeople, itemType):
+    
+
+
+
+
