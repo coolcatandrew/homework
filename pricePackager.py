d7205fa80e133e72303701ac49c6e8284a9f9ee9
coolcatandrew <liandrew@live.com>
2017-11-03 12:58:31 -0400

    corrections

--- pricePackager.py
+++ pricePackager.py
@@ -13,18 +13,27 @@ def calculateMarkup(basePrice, numberOfPeople, itemType):
     if (not isinstance(basePrice, float)):
         print("error - basePrice is not a float")
         return -1
+    
     if (not numberOfPeople[0:-7].isdigit()):
         print("error - please enter the number of people needed in the format (# people)")
         return -1
+    
     subtotal = float(basePrice*1.05)
+    
     print(subtotal)
-    markup = 0.00
-    if (itemType.lower() == "pharmaceutical"):
-        markup = 0.075
-    elif (itemType.lower() == "food"):
-        markup = 0.13
-    elif (itemType.lower() == "books"):
-        markup = 0.05
+    
+    peopleMarkup = 0
 
+    
+    typeMarkup = 0.00
+
+    if (itemType.lower() == "pharmaceutical"):
+        typeMarkup = 0.075
+    elif (itemType.lower() == "food"):
+        typeMarkup = 0.13
+    elif (itemType.lower() == "electronic"):
+        typeMarkup = 0.02
+
+    print(typeMarkup * subtotal)
 
 calculateMarkup(1299.99,"3 people", "food")
